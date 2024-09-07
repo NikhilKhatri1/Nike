f0ce204063e0dbcac8a283e4286243a35cc17d57
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-09-08 00:53:55 +0530

    Files Added

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,14 @@
+import { services } from "../Constants/index";
+import { ServiceCard } from "../Components/ServiceCard";
 
-const Services = () => {
+export const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <section className='flex flex-wrap justify-center max-container gap-9'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
 
-export default Services
+export default Services;
